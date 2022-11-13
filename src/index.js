92d8e37ce896fc3810b703441b4dbee3b4fadc29
ZhykharievAnatolii <nekijt87@gmail.com>
2022-11-13 20:15:28 +0200

    update index js
    ver of hw

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import { Client } from 'pg';
+import {
+  Client
+} from 'pg';
 
 export const initConnection = () => {
   const {
@@ -23,8 +25,62 @@ export const createStructure = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
-  // Your code is here...
+  await client.query(`
+  CREATE TABLE users
+  (
+    id serial PRIMARY KEY,
+    name VARCHAR (30) NOT NULL,
+    date TIMESTAMP DEFAULT now()
+    );
+    `);
+  await client.query(
+    `CREATE TABLE categories
+  (
+    id serial PRIMARY KEY,
+    name VARCHAR (30) NOT NULL
+    );`
+  );
+  await client.query(
+    `CREATE TABLE authors
+  (
+    id serial PRIMARY KEY,
+    name VARCHAR (30) NOT NULL
+    );`
+  );
+  await client.query(
+    `CREATE TABLE books
+    (
+      id serial PRIMARY KEY,
+      title VARCHAR(30) NOT NULL,
+      userid INTEGER NOT NULL,
+      authorid INTEGER NOT NULL,
+      categoryid INTEGER NOT NULL,
+      FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE,
+      FOREIGN KEY (authorid) REFERENCES authors(id) ON DELETE CASCADE,
+      FOREIGN KEY (categoryid) REFERENCES categories(id) ON DELETE CASCADE
+      );`
+  );
+  await client.query(
+    `CREATE TABLE descriptions
+    (
+      id serial PRIMARY KEY,
+      description VARCHAR (10000) NOT NULL,
+      bookid INTEGER UNIQUE NOT NULL,
+      FOREIGN KEY (bookid) REFERENCES books(id) ON DELETE CASCADE
+      );`
+  );
+  await client.query(
+    `CREATE TABLE reviews
+    (
+      id serial PRIMARY KEY,
+      message VARCHAR (10000) NOT NULL,
+      userid INTEGER NOT NULL,
+      bookid INTEGER NOT NULL,
+      FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE,
+      FOREIGN KEY (bookid) REFERENCES books(id) ON DELETE CASCADE
+      );`
+  );
+
 
   client.end();
 };
@@ -33,7 +89,36 @@ export const createItems = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
+  await client.query(`
+  INSERT INTO users (name, date)
+  VALUES ('Marry', '2022-07-07 23:54:01');
+`);
+
+  await client.query(`
+  INSERT INTO categories (name)
+  VALUES ('Horror');
+`);
+
+  await client.query(`
+  INSERT INTO authors (name)
+  VALUES ('Bram Stoker');
+`);
+
+  await client.query(`
+  INSERT INTO books (title, userid, authorid, categoryid)
+  VALUES ('Dracula', 1, 1, 1);
+`);
+
+  await client.query(`
+  INSERT INTO descriptions (description, bookid)
+  VALUES ('This book tells about struggling between Good and Evil, about endless war between science and supernatural, about power of money and weakness of human's flesh, and, of cource, about love and passion.', 1);
+`);
+
+  await client.query(`
+  INSERT INTO reviews (message, userid, bookid)
+  VALUES ('It's a timeless masterpiece of horror literature I can read over and over again. Actually, so I do, maybe, once a year.', 1, 1);
+`);
+
 
   client.end();
 };
